f13db14579be3c6f102911d245f4678dcb080531
Farouk Elayache <farouk@MacBook-Pro-de-Farouk.local>
2021-01-16 14:23:22 +0100

    correction of a bug

--- hangman.py
+++ hangman.py
@@ -13,7 +13,8 @@ Do Voulez vous jouer Y/N
         Demander à l'utilisateur seulement un caractere,
 
     Convertir ce caractere
-    Si la lettre existe dans le mot :
+    Si la lettre existe 
+    word_to_find le mot :
         afficher le mot caché mise à jour avec l'emplacement
     SiNo
         Afficher la potence corespendante
@@ -31,7 +32,8 @@ Do Voulez vous jouer Y/N
 
     Définir les variables
     fonction recupérer le mot
-    fonction vérifier si la lettre existe dans le mot
+    fonction vérifier si la lettre existe 
+    word_to_find le mot
     fonction récuperer la lettre
     fonction (nbre d'eche)
         print potence
@@ -41,7 +43,7 @@ Do Voulez vous jouer Y/N
 
 #r = RandomWords()
 
-#r.get_random_word()
+# r.get_random_word()
 '''
 nbr_letter_to_find = 5
 nbr_fail = 0
@@ -62,29 +64,42 @@ while nbr_fail < 5 or len(nbr_letter_to_find) != len(word_to_find):
         break
 
 '''
-f = open("hang.py","r")
-content = f.read()
-print(content)
+import logging
+import datetime
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+log_format = '%(asctime)s %(filename)s: %(message)s'
+logging.basicConfig(filename="hangman.log", format=log_format,
+                    datefmt='%Y-%m-%d %H:%M:%S' )
+
+logger.info("information message")
+ 
+
+f = open("hang.py", "r")
+stages = f.read()
+print(stages)
 
 
 guesses = []
 count = 1
-word_to_find = 'python'
+word_to_find = "python"
 word = []
 
-while count < 10:
+while count < 7:
     guess = input('guess a letter: ')
     guesses.append(guess)
+    print(guesses)
     if ''.join(word) == word_to_find:
         print('you win')
         break
-    elif len(guess) > 1 and word_to_find == guess:
+    elif len(word_to_find) == len(guesses):
         print(word_to_find)
         print('you win')
         break
     else:
         for char in word_to_find:
-            if char in guesses and len(stages) - 1:
+            if char in guesses:
                 word.append(char)
                 print(char)
             else:
@@ -92,3 +107,6 @@ while count < 10:
         count += 1
 else:
     print('\nyou lose')
+
+
+
